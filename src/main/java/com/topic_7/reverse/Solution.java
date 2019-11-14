2617dd82c51ecfdd9bfdef4101528e3d1baf81df
lq <1911743981@qq.com>
2019-11-14 09:51:25 +0800

    topic_7 - 整数反转

---
+++ src/main/java/com/topic_7/reverse/Solution.java
@@ -0,0 +1,42 @@
+package com.topic_7.reverse;
+
+/**
+ * 给出一个 32 位的有符号整数，你需要将这个整数中每位上的数字进行反转。
+ *
+ * 示例 1:
+ *
+ * 输入: 123
+ * 输出: 321
+ *  示例 2:
+ *
+ * 输入: -123
+ * 输出: -321
+ * 示例 3:
+ *
+ * 输入: 120
+ * 输出: 21
+ * 注意:
+ *
+ * 假设我们的环境只能存储得下 32 位的有符号整数，则其数值范围为 [−2^31,  2^31 − 1]。请根据这个假设，如果反转后整数溢出那么就返回 0。
+ * @author aixiaobai
+ * @date 2019/11/13 17:33
+ */
+public class Solution {
+    public int reverse(int x) {
+        String s = Integer.toString(x);
+        char[] chars = s.toCharArray();
+        boolean flag = chars[0] == '-'?false:true;
+        if(flag){
+            s = new StringBuffer(s).reverse().toString();
+        }else{
+            s = new StringBuffer(s.substring(1)).reverse().toString();
+            s="-"+s;
+        }
+        long l = Long.parseLong(s);
+        if(l>=Integer.MIN_VALUE && l<= Integer.MAX_VALUE){
+            return (int)l;
+        }else{
+            return 0;
+        }
+    }
+}
---
+++ src/test/java/com/topic_7/reverse/SolutionTest.java
@@ -0,0 +1,18 @@
+package com.topic_7.reverse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author aixiaobai
+ * @date 2019/11/13 17:44
+ */
+public class SolutionTest {
+
+    @Test
+    public void reverse() {
+        Solution solution = new Solution();
+        System.out.println(solution.reverse(-255));
+    }
+}
